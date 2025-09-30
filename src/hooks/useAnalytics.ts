5b78dcfb5c9a8092fcacf24c006d72e6439ad2df
Barbaros Zongur <barbaroszongur22@gmail.com>
2025-09-30 17:39:21 +0300

    Add UTM tracking script to index.html and refactor useAnalytics hook to streamline UTM parameter handling

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -16,36 +16,11 @@ export const useAnalytics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    console.log('Analytics initialized');
-
     // Send page view to Google Analytics
     if (window.gtag) {
       window.gtag('config', 'G-7RY3DM815X', {
         page_path: location.pathname + location.search,
       });
-    }
-
-    // Check for UTM parameters
-    const searchParams = new URLSearchParams(location.search);
-    const utmSource = searchParams.get('utm_source');
-    const utmMedium = searchParams.get('utm_medium');
-    const utmCampaign = searchParams.get('utm_campaign');
-
-    // If UTM parameters exist, send custom event
-    if (utmSource || utmMedium || utmCampaign) {
-      console.log('UTM parameters detected:', {
-        utm_source: utmSource,
-        utm_medium: utmMedium,
-        utm_campaign: utmCampaign,
-      });
-
-      if (window.gtag) {
-        window.gtag('event', 'campaign_tracking', {
-          campaign_source: utmSource,
-          campaign_medium: utmMedium,
-          campaign_name: utmCampaign,
-        });
-      }
     }
   }, [location]);
 };
